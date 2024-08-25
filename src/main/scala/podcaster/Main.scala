aced9e2911027cb62aeaf9fd7c9f42bf6325c7a7
Salil Wadnerkar <bsdprg@tuta.io>
2024-08-25 12:02:28 -0700

    Added error handling

--- src/main/scala/podcaster/Main.scala
+++ src/main/scala/podcaster/Main.scala
@@ -42,107 +42,104 @@ def extractFeed(xmlString: String): Seq[Episode] = {
   // convert the `String` to a `scala.xml.Elem`
   val xml = XML.loadString(xmlString)
   val rssItems = xml \ "channel" \ "item"
-  val episodes = for {
+  for {
     i <- rssItems
     title = (i \ "title").text
     url = i \ "enclosure" \@ "url"
     pubDate = (i \ "pubDate").text
   } yield Episode(title, url, pubDate)
-  episodes
+}
+
+// Handle 302 redirect responses while getting responses. As per the standard, we need to explicitly decide to follow the redirect URL.
+def handle302Response(redirectUrl: Option[String], podcastId: String, podcastDirPath: Path): Future[Path] = {
+  redirectUrl.map(url => {
+    logger.debug(s"$podcastId: redirecting to $url")
+    val downloadUri = new URI(url)
+    val downloadFileName = Paths.get(downloadUri.getPath).getFileName
+    val downloadPath = podcastDirPath.resolve(downloadFileName)
+    val request = basicRequest
+      .get(uri"${downloadUri}")
+      .response(asPath(downloadPath))
+    for response <- request.send(backend)
+      yield response.body match {
+        case Right(body) => body
+        case Left(s) => throw new RuntimeException(s)
+      }
+  }).getOrElse(Future { throw new RuntimeException("No redirect URL!") })
 }
 
 // Download the podcast episode
-def downloadEpisode(podcastId: String, episode: Episode, podcastDirPath: Path): Future[Either[String, Path]] = {
+def downloadEpisode(podcastId: String, episode: Episode, podcastDirPath: Path): Future[Path] = {
   val uri = new URI(episode.url)
   // Remove the query params and the fragment from the URI
-  var downloadUri = new URI(
-    uri.getScheme, // scheme
-    null, // user info
-    uri.getHost,
-    uri.getPort,
-    uri.getPath,
-    null, // query
-    null) // fragment
-
-  var downloadFileName = Paths.get(uri.getPath).getFileName
-  var downloadPath = podcastDirPath.resolve(downloadFileName)
+  val downloadUri = new URI(uri.getScheme, null, uri.getHost, uri.getPort, uri.getPath, null, null)
+  val downloadFileName = Paths.get(downloadUri.getPath).getFileName
+  val downloadPath = podcastDirPath.resolve(downloadFileName)
   if !Files.exists(downloadPath) then
     logger.debug(s"$podcastId: downloading episode ${episode.title} published at ${episode.pubDate} from $downloadUri")
-    var request = basicRequest.get(uri"$downloadUri")
-                              .response(asPath(downloadPath))
-    request.send(backend).flatMap(response => {
-      val redirectUrl = response.header(HeaderNames.Location)
-      if response.code.isRedirect && redirectUrl.isDefined then
-        logger.debug(s"$podcastId: redirecting to ${redirectUrl.get}")
-        redirectUrl.map(url => {
-          downloadUri = new URI(redirectUrl.get)
-          downloadFileName = Paths.get(downloadUri.getPath).getFileName
-          downloadPath = podcastDirPath.resolve(downloadFileName)
-          request = basicRequest.get(uri"${downloadUri}")
-                                .response(asPath(downloadPath))
-          request.send(backend).map(_.body)
-        }).getOrElse(Future { Left("No redirect location found") })
-      else
-        Future { response.body }
-    })
+    var request = basicRequest
+      .get(uri"$downloadUri")
+      .response(asPath(downloadPath))
+    request.send(backend).flatMap {
+      response => if response.code.isRedirect then handle302Response(response.header(HeaderNames.Location), podcastId, podcastDirPath)
+                  else Future {
+                    response.body match {
+                      case Right(body) => body
+                      case Left(s) => throw new RuntimeException(s)
+                    }
+                  }
+    }
   else
     logger.debug(s"$podcastId: episode ${episode.title} already downloaded, skipping..")
-    Future { Right(downloadPath) }
+    Future { downloadPath }
 }
 
-def sequence[A, B](s: Seq[Either[A, B]]): Either[A, Seq[B]] =
-  s.foldRight(Right(Nil): Either[A, List[B]]) {
-    (e, acc) => for (xs <- acc; x <- e) yield x :: xs
-  }
-
 // Downloads <count> episodes of the podcast
-def downloadEpisodes(podcastId: String, episodes: Seq[Episode], podcastDirPath: Path, count: Int = 1): Future[Either[String, Seq[Path]]] = {
-  val episodeFutures = episodes
-                          .take(count)
-                          .map(episode => downloadEpisode(podcastId, episode, podcastDirPath))
-  Future.sequence(episodeFutures).map(fs => sequence(fs))
+def downloadEpisodes(podcastId: String, episodes: Seq[Episode], podcastDirPath: Path, count: Int = 1): Future[Seq[Path]] = {
+  Future.traverse(episodes.take(count)) {
+    episode => downloadEpisode(podcastId, episode, podcastDirPath).recover {
+      case e: Exception => {
+        logger.error(s"$podcastId: episode ${episode.title} could not be downloaded due to ${e.getMessage}")
+        null
+      }
+    }
+  }
 }
 
 // Checks the podcast feed and returns episodes
-def downloadPodcastFeed(podcastUrl: String): Future[Either[String, Seq[Episode]]] = {
-  val request = basicRequest.get(uri"$podcastUrl")
+def downloadPodcastFeed(podcastUrl: String): Future[Seq[Episode]] = {
+  val request = basicRequest
+    .get(uri"$podcastUrl")
     .response(asString("utf-8"))
-  request.send(backend).map(response => response.body.map(xmlString => extractFeed(xmlString)))
+  request.send(backend).map(response => response.body match {
+    case Right(xmlString) => extractFeed(xmlString)
+    case Left(s) => throw new RuntimeException(s)
+  })
 }
 
 // Checks the podcast feed, and downloads the episodes
-def downloadPodcast(podcastId: String, podcastUrl: String, mediaDir: Path): Future[Either[String, Seq[Path]]] = {
-  downloadPodcastFeed(podcastUrl)
-    .flatMap(feed => {
-      val podcastDirPath = mediaDir.resolve(podcastId)
-      if !Files.exists(podcastDirPath) then
-        Files.createDirectory(podcastDirPath)
-      // downloadResult is of type Either[String, Future[Either[String, Seq[Path]]]]
-      // We need to return the type Future[Either[String, Seq[Path]]]
-      val downloadResult = feed.map(episodes => downloadEpisodes(podcastId, episodes, podcastDirPath, 3))
-      downloadResult match {
-        case Left(s) => Future { Left(s) }
-        case Right(r) => r
-      }
-    })
+def downloadPodcast(podcastId: String, podcastUrl: String, mediaDir: Path): Future[Seq[Path]] = {
+  val podcastDirPath = mediaDir.resolve(podcastId)
+  if !Files.exists(podcastDirPath) then Files.createDirectory(podcastDirPath)
+  downloadPodcastFeed(podcastUrl) flatMap {
+    episodes => downloadEpisodes(podcastId, episodes, podcastDirPath, 3)
+      .filter(p => p != null)
+  }
 }
 
 
-@main def podcaster(): Unit = {
-  parseConfig() match {
-    case Failure(e) => logger.debug(s"Failed to parse config $e")
-    case Success(config) =>
-      if !Files.exists(config.mediaDir) then
-        Files.createDirectory(config.mediaDir)
-      // podcastFutures is of type Seq[Future[Either[String, Seq[Path]]]]
-      val podcastsFutures = config.podcastEntries
-        .map((podcastId, podcastUrl) => downloadPodcast(podcastId, podcastUrl, config.mediaDir)
-          .map {
-            case Left(e) => logger.info(s"$podcastId: Got an error $e while downloading podcasts")
-            case Right(ps) => logger.info(s"$podcastId: Check the files ${ps.map(_.getFileName).mkString(", ")}")
-          })
-      // resultFuture is of type Future[Seq[Either[String, Seq[Path]]]]
-      val resultFuture = Future.sequence(podcastsFutures)
-      Await.result(resultFuture, Duration.Inf)
-  }
+@main def podcaster(): Unit = parseConfig() match {
+  case Failure(e) => logger.debug(s"Failed to parse config $e")
+  case Success(config) =>
+    if !Files.exists(config.mediaDir) then Files.createDirectory(config.mediaDir)
+    val resultFuture = Future.traverse(config.podcastEntries) {
+      (podcastId, podcastUrl) => downloadPodcast(podcastId, podcastUrl, config.mediaDir)
+        .map { paths => logger.info(s"$podcastId: Check the files ${paths.map(_.getFileName).mkString(", ")}") }
+        .recover { case e: Exception => {
+            logger.error(s"$podcastId: Got an error ${e.getMessage} while downloading podcasts")
+            ""
+          }
+        }
+    }
+    Await.result(resultFuture, Duration.Inf)
 }
